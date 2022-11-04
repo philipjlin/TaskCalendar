e417d628fae1fd00eb3da59945d4bcf284293306
Philip Lin <phil@Philips-MacBook-Air.local>
2022-11-03 18:04:50 -0700

    Linked to Mongodb.

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 /*
- * Main server with routes
+ * Main server
  */
 
 //Required packages
-let path = require('path');
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const countModule = require(path.join(__dirname, "countModule.js"));  //Required local module
 
 const app = express();
@@ -19,26 +20,50 @@ app.use(bodyParser.urlencoded({extended: true}));
 //Allows use of static files in public directory (css, images)
 app.use(express.static(path.join(__dirname, "public")));
 
+//Mongoose
+//mongoose.connect("mongodb://localhost:27017/tasklist");
+mongoose.connect("mongodb+srv://admin:adminpassword@philipjlin.9wd1zfh.mongodb.net/tasklist");
 
 
 
 
+
+/******************** DATABASE CODE ********************/
 /*
- * App variables
+ * Task DOCUMENTS are stored in tasks COLLECTION in tasklist DB
+ * Each task has day of the week and task name
+ *
  */
-//Lists for each day of the week
-let mondayList = ["Fill out this list"];
-let tuesdayList = ["Fill out this list"];
-let wednesdayList = ["Fill out this list"];
-let thursdayList = ["Fill out this list"];
-let fridayList = ["Fill out this list"];
-let saturdayList = ["Fill out this list"];
-let sundayList = ["Fill out this list"];
+//Schema for mongodb database
+const taskSchema = new mongoose.Schema({
+
+  day: String,
+  task: {
+    type: String,
+    maxLength: 32
+  }
+});
+
+//Model for mongodb database
+const Task = mongoose.model("Task", taskSchema);
+
+//(Sample) document to add to database
+let sampleTask = new Task({
+
+  day: "monday",
+  task: "This is a sample task."
+});
+
+
+
+/******************** APP VARIABLES ********************/
 
 
 
 
 
+
+/******************** SERVER ROUTES ********************/
 /*
  * LISTEN route
  * process.env.port listens for Heroku server
@@ -58,17 +83,25 @@ app.listen(process.env.PORT || "3000", function(req, res){
  *
  */
 app.get("/", function(req, res){
-  //Client side dateScript will reroute to current day
+
+  //NOTE: client-side dateScript.js will reroute default route to current day route using client time zone
+
   res.render("list", {listTitle: "", taskList: [], taskCount: 0});
 });
+
+
 
 //Render based on query parameter day
 app.get("/:day", function(req, res){
 
   let day = req.params.day;
-  let list = eval(day + "List");
 
-  res.render("list", {listTitle: day[0].toUpperCase()+day.substring(1), taskList: list, taskCount: countModule.countTasks(list) });
+  //Find tasks in database of certain day
+  Task.find({ day:day }, function(err, foundTasks){
+
+    res.render("list", {listTitle: day[0].toUpperCase()+day.substring(1), taskList: foundTasks, taskCount: countModule.countTasks(foundTasks) });
+  });
+
 });
 
 
@@ -79,29 +112,44 @@ app.get("/:day", function(req, res){
  * POST routes
  *
  */
+//Post request from add submit button
 app.post("/", function(req, res){
 
-  // POST request came from add submit button
-  if( req.body.addList != null ){
+  //Get info from the request
+  let requestList = req.body.addList.toLowerCase();  //req.body.addList is list name from submit button with name "addList"
+  let requestTask = req.body.addTask;                //req.body.addTask is task from input field with name "addTask"
 
-    let currentList = req.body.addList;  //req.body.addList from submit button with name "addList"
-    let listToPass = eval(currentList.toLowerCase() + "List"); //Obtain in-use list array from submit button value in POST req
+  //Create task document to save to database
+  let taskToAdd = new Task({
 
-    listToPass.push(req.body.task); //Add task from input of POST req form to in-use list
+    day: requestList,
+    task: requestTask
+  });
 
-    res.redirect("/" + currentList.toLowerCase()); //Redirect to view
-  }
+  taskToAdd.save();
+
+  res.redirect("/" + requestList); //Redirect to view
+
+});
 
 
-  // POST request came from remove submit button
-  if( req.body.remove != null ){
 
-    let currentList = req.body.removeList; //req.body.removeList from submit button with name "removeList"
-    let listToPass = eval(currentList.toLowerCase() + "List"); //Obtain in-use list array from submit button value in POST req
+// POST request from remove submit button
+app.post("/remove", function(req, res){
 
-    let indexToRemove = listToPass.indexOf(req.body.remove); //Find and remove from in-use list task in POST req associated with clicked submit button
-    listToPass.splice(indexToRemove, 1);
+  if( req.body.removeId != null ){
 
-    res.redirect("/" + currentList.toLowerCase()); //Redirect to view
+    let requestList = req.body.removeList.toLowerCase();  //req.body.removeList is list name from hidden field of form
+    let requestTaskId = req.body.removeId;                //req.body.removeId is task id from submit button with name "removeId"
+
+    Task.findByIdAndRemove(requestTaskId, function(err){
+
+      if(err)
+        console.log(err);
+      else
+        console.log("Task Removed.");
+    });
+
+    res.redirect("/" + requestList); //Redirect to view
   }
 });
